0b54ebefd6d8039db31375849b152c2f26c2f5f3
Matt Winter <MattWinter@gmail.com>
2020-08-12 10:34:24 -0400

    Replace alt-click with simpler dconf write

--- import-dconf.sh
+++ import-dconf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
+dconf write /org/gnome/desktop/wm/preferences/mouse-button-modifier '"<Alt>"'
 dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
-dconf load /org/gnome/desktop/wm/preferences/ < alt-click.dconf
